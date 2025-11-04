32202a42efa417d7e29207c41a5536c90cc5a76c
Komal Rathore <komalrathore0521@gmail.com>
2025-11-04 11:39:15 +0530

    feat(ride): Create RideRequestDto for requesting a ride

---
+++ src/main/java/com/project/uber/Uber/dto/RideRequestDto.java
@@ -0,0 +1,97 @@
+package com.project.uber.Uber.dto;
+
+
+import com.project.uber.Uber.entities.enums.PaymentMethod;
+import com.project.uber.Uber.entities.enums.RideRequestStatus;
+
+import java.time.LocalDateTime;
+
+public class RideRequestDto {
+
+    private Long id;
+    private PointDto pickUpLocation;
+    private PointDto dropOffLocation;
+    private LocalDateTime requestTime;
+    private RiderDto rider;
+    private Double fare;
+    private PaymentMethod paymentMethod;
+    private RideRequestStatus status;
+
+    public RideRequestDto() {
+    }
+
+    public RideRequestDto(Long id, PointDto pickUpLocation, Double fare, PointDto dropOffLocation, LocalDateTime requestTime, RiderDto rider, PaymentMethod paymentMethod, RideRequestStatus status) {
+        this.id = id;
+        this.pickUpLocation = pickUpLocation;
+        this.dropOffLocation = dropOffLocation;
+        this.requestTime = requestTime;
+        this.fare = fare;
+        this.rider = rider;
+        this.paymentMethod = paymentMethod;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public PointDto getPickUpLocation() {
+        return pickUpLocation;
+    }
+
+    public void setPickUpLocation(PointDto pickUpLocation) {
+        this.pickUpLocation = pickUpLocation;
+    }
+
+    public PointDto getDropOffLocation() {
+        return dropOffLocation;
+    }
+
+    public void setDropOffLocation(PointDto dropOffLocation) {
+        this.dropOffLocation = dropOffLocation;
+    }
+
+    public LocalDateTime getRequestTime() {
+        return requestTime;
+    }
+
+    public void setRequestTime(LocalDateTime requestTime) {
+        this.requestTime = requestTime;
+    }
+
+    public RiderDto getRider() {
+        return rider;
+    }
+
+    public void setRider(RiderDto rider) {
+        this.rider = rider;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public RideRequestStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(RideRequestStatus status) {
+        this.status = status;
+    }
+
+    public Double getFare() {
+        return fare;
+    }
+
+    public void setFare(Double fare) {
+        this.fare = fare;
+    }
+}
